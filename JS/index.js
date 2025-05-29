f9fe94f75d60f539e5b276910e56369ae3cbca80
Jocelyn Ramirez-Parada <joc3lyn.r@gmail.com>
2025-05-28 20:05:27 -0700

    cat only

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,20 @@
-fetch('https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_AKvUElDcWTTellUdqBRSKv4hXbHBoPfQgwimWMC42bhWIPVPviraRMjlnv2Nr8Tt')
-    .then((response) => {
-            if (!response.ok) {
-            throw new Error('Request failed');
-            }
-            return response.json();
-    })
-    .then((data) => {;
-        console.log(data);
+//Cat Data
 
-    })
-    .catch((error) => {
-            console.error('An error occurred:', error);
-    });
+fetch('https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_AKvUElDcWTTellUdqBRSKv4hXbHBoPfQgwimWMC42bhWIPVPviraRMjlnv2Nr8Tt')
+        .then((response) => {
+                if (!response.ok) {
+                        throw new Error('Request failed');
+                }
+                return response.json();
+        })
+        .then((data) => {
+                ;
+                console.log(data);
+
+        })
+        .catch((error) => {
+                console.error('An error occurred:', error);
+        });
+
+        
+
